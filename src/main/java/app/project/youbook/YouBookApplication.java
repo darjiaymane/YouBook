f7484365fb1248daa0698e67987fbda067e91d26
Darji Aymane <aymanedarji7@gmail.com>
2022-12-28 09:33:02 +0100

    Initial commit

---
+++ src/main/java/app/project/youbook/YouBookApplication.java
@@ -0,0 +1,13 @@
+package app.project.youbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/project/youbook/YouBookApplicationTests.java
@@ -0,0 +1,13 @@
+package app.project.youbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
