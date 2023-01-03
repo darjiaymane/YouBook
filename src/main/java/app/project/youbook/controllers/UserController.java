639273225b3559da7f42f0e1db8a2a66392aebce
Darji Aymane <aymanedarji7@gmail.com>
2023-01-03 11:45:17 +0100

    Add find all users to UserController

--- src/main/java/app/project/youbook/controllers/UserController.java
+++ src/main/java/app/project/youbook/controllers/UserController.java
@@ -1,7 +1,20 @@
 package app.project.youbook.controllers;
 
+import app.project.youbook.services.Dto.ResponseDto;
+import app.project.youbook.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("users")
 public class UserController {
+    @Autowired
+    UserService userService;
+
+    @GetMapping
+    public ResponseDto findAllUsers(){
+        return userService.findAll();
+    }
 }
