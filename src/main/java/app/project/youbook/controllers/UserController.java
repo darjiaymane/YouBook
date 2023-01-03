af19cb16cd6227b6b4285eddf637f98fb38cc88e
Darji Aymane <aymanedarji7@gmail.com>
2023-01-03 14:35:38 +0100

    Autowire the userService controller

--- src/main/java/app/project/youbook/controllers/UserController.java
+++ src/main/java/app/project/youbook/controllers/UserController.java
@@ -10,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("users")
 public class UserController {
-    @Autowired
-    UserService userService;
+    final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping
     public ResponseDto findAllUsers(){
