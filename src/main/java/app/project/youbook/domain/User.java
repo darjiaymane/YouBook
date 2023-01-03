9d7ebfaad9b2ebee9d30b740eb6e5e9adad2e9ec
Darji Aymane <aymanedarji7@gmail.com>
2023-01-03 09:30:31 +0100

    Create the class User with constructors, getter, setters and the relation with role

---
+++ src/main/java/app/project/youbook/domain/User.java
@@ -0,0 +1,31 @@
+package app.project.youbook.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user")
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+    private String password;
+    //Active - Banned
+    private String status;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private List<Role> roles;
+}
