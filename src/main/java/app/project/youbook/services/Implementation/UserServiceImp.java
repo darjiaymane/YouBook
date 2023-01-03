34fcf72fbc00f4e3eda4ef0970d0d4ceb4e9bbe4
Darji Aymane <aymanedarji7@gmail.com>
2023-01-03 11:48:51 +0100

    Add Implements userService methods: -findAll -findByFirstName -findByStatus -findByEmail -findByUsername -findById -Save

--- src/main/java/app/project/youbook/services/Implementation/UserServiceImp.java
+++ src/main/java/app/project/youbook/services/Implementation/UserServiceImp.java
@@ -1,6 +1,118 @@
 package app.project.youbook.services.Implementation;
 
+import app.project.youbook.services.Dto.ResponseDto;
+import app.project.youbook.Enum.UserStatus;
+import app.project.youbook.domain.User;
+import app.project.youbook.repositories.UserRepository;
+import app.project.youbook.services.Dto.UserDto;
 import app.project.youbook.services.UserService;
+import org.apache.tomcat.util.http.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.swing.text.html.Option;
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 
 public class UserServiceImp implements UserService {
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    ResponseDto responseDTO;
+    @Override
+    public ResponseDto findAll(){
+        List<User> userList = userRepository.findAll();
+        responseDTO.setStatus("200");
+        if (userList.size() == 0){
+            responseDTO.setMessage("No user found");
+            return responseDTO;
+        }
+        responseDTO.setMessage("Users List");
+        responseDTO.setData(userList);
+        return responseDTO;
+    }
+    @Override
+    public ResponseDto save(User user) {
+        if (user == null || user == new User()){
+
+            responseDTO.setStatus("404");
+            responseDTO.setMessage("All Client information's are mandatory");
+            return responseDTO;
+        }
+        userRepository.save(user);
+        responseDTO.setStatus("200");
+        responseDTO.setMessage("Client has been added successfully");
+        responseDTO.setData(user);
+        return responseDTO;
+    }
+
+    @Override
+    public ResponseDto findByStatus(UserStatus status){
+        User user = userRepository.findByStatus(status);
+        if (user == null){
+            responseDTO.setStatus("200");
+            responseDTO.setMessage("There is no User with this status");
+            return responseDTO;
+        }
+        responseDTO.setData(user);
+        return responseDTO;
+    }
+
+    @Override
+    public ResponseDto findByEmail(String email) {
+        User user = userRepository.findByEmail(email);
+        if (user == null){
+            responseDTO.setStatus("404");
+            responseDTO.setMessage("User Not found");
+            return responseDTO;
+        }
+        responseDTO.setData(user);
+        return responseDTO;
+    }
+
+    @Override
+    public ResponseDto findByUsername(String username) {
+        User user = userRepository.findByUsername(username);
+        if (user == null){
+            responseDTO.setStatus("404");
+            responseDTO.setMessage("User not found");
+            return responseDTO;
+        }
+        responseDTO.setData(user);
+        return responseDTO;
+    }
+
+    @Override
+    public ResponseDto findById(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        if (!user.isPresent()){
+            responseDTO.setStatus("200");
+            responseDTO.setMessage("User not found");
+            return responseDTO;
+        }
+        responseDTO.setData(user);
+        return responseDTO;
+    }
+
+    @Override
+    public ResponseDto findByFirstName(String firsName) {
+        User user = userRepository.findByFirstName(firsName);
+        if (user == null){
+            responseDTO.setStatus("200");
+            responseDTO.setMessage("User not found");
+            return responseDTO;
+        }
+        responseDTO.setData(user);
+        return responseDTO;
+    }
+    @Transactional
+    @Override
+    public ResponseDto update(User user) {
+        return null;
+    }
+
+    @Override
+    public ResponseDto Delete(Long id) {
+        return null;
+    }
 }
