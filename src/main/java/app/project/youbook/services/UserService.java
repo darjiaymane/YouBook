2952b3a15b0c61a37f098ba58ca0634b3541aa1f
Darji Aymane <aymanedarji7@gmail.com>
2023-01-03 10:22:41 +0100

    Create UserService interface with methods

--- src/main/java/app/project/youbook/services/UserService.java
+++ src/main/java/app/project/youbook/services/UserService.java
@@ -1,8 +1,17 @@
 package app.project.youbook.services;
 
+import app.project.youbook.Enum.UserStatus;
+import app.project.youbook.domain.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface UserService {
-
+    User save(User user);
+    User findByStatus(UserStatus Status);
+    User findByEmail(String email);
+    User findByUsername(String username);
+    User findById();
+    User findByFirstName(String firsName);
+    User update(User user);
+    User Delete(Long id);
 }
