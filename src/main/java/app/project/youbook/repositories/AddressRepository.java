714bf53908e6cbf97a6dfceb0fe25861e0d5cc44
Darji Aymane <aymanedarji7@gmail.com>
2023-01-04 09:41:17 +0100

    Create Address repository

---
+++ src/main/java/app/project/youbook/repositories/AddressRepository.java
@@ -0,0 +1,9 @@
+package app.project.youbook.repositories;
+
+import app.project.youbook.domain.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
