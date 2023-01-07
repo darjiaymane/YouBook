fc987c221bcefd1aa6080e0cf8467931195b6810
Darji Aymane <aymanedarji7@gmail.com>
2023-01-07 23:50:41 +0100

    Make the username and the email unique

--- src/main/java/app/project/youbook/domain/User.java
+++ src/main/java/app/project/youbook/domain/User.java
@@ -1,5 +1,8 @@
 package app.project.youbook.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +17,7 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name="userEntity")
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,12 +25,15 @@ public class User implements Serializable {
 
     private String firstName;
     private String lastName;
+    @Column(unique = true)
     private String username;
+    @Column(unique = true)
     private String email;
     private String password;
     //Active - Banned
     private String status;
-    @ManyToMany
+
+    @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(name = "user_entity_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "roles_id"))
@@ -37,6 +44,31 @@ public class User implements Serializable {
 
     @OneToMany(mappedBy = "user", orphanRemoval = true)
     private Set<Reservation> reservations = new LinkedHashSet<>();
+    @JsonIgnore
+    public Set<Hotel> getHotels() {
+        return hotels;
+    }
+    @JsonProperty
+    public void setHotels(Set<Hotel> hotels) {
+        this.hotels = hotels;
+    }
+
+    public Set<Reservation> getReservations() {
+        return reservations;
+    }
+
+    public void setReservations(Set<Reservation> reservations) {
+        this.reservations = reservations;
+    }
+
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 
 
 }
