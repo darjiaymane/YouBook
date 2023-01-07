bc6b540d3baa9a980828849baee9ba898754d2c7
Darji Aymane <aymanedarji7@gmail.com>
2023-01-07 23:36:18 +0100

    Make the Many to many relation between role and user unidirectional

--- src/main/java/app/project/youbook/domain/Role.java
+++ src/main/java/app/project/youbook/domain/Role.java
@@ -1,9 +1,13 @@
 package app.project.youbook.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -20,14 +24,4 @@ public class Role implements Serializable {
     private Long id;
 
     private String name;
-    @ManyToMany(mappedBy = "roles")
-    private Set<User> users = new LinkedHashSet<>();
-
-    public Set<User> getUsers() {
-        return users;
-    }
-
-    public void setUsers(Set<User> users) {
-        this.users = users;
-    }
 }
