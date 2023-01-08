fbabf8f6f6f06bb99a8393d7ad38338eeaf634d2
Darji Aymane <aymanedarji7@gmail.com>
2023-01-08 01:48:27 +0100

    Create the admin controller and add the AdminController.createHotel and AdminController.updateHotel methods

--- src/main/java/app/project/youbook/controllers/AdminController.java
+++ src/main/java/app/project/youbook/controllers/AdminController.java
@@ -1,2 +1,36 @@
-package app.project.youbook.controllers;public class AdminController {
+package app.project.youbook.controllers;
+
+import app.project.youbook.domain.Hotel;
+import app.project.youbook.services.Dto.ResponseDto;
+import app.project.youbook.services.HotelService;
+import app.project.youbook.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+    private final HotelService hotelService;
+    private final UserService userService;
+
+    public AdminController(HotelService hotelService, UserService userService) {
+        this.hotelService = hotelService;
+        this.userService = userService;
+    }
+
+    @PostMapping("/hotels")
+    public ResponseEntity<ResponseDto> createHotel(@RequestBody Hotel hotel) {
+        ResponseDto createdHotel = hotelService.save(hotel);
+        return ResponseEntity.ok(createdHotel);
+    }
+
+
+    @PutMapping("/hotels/{id}")
+    public ResponseEntity<ResponseDto> updateHotel(@PathVariable Long id, @RequestBody Hotel hotel) {
+        ResponseDto updatedHotel = hotelService.update(id, hotel);
+        return ResponseEntity.ok(updatedHotel);
+    }
 }
