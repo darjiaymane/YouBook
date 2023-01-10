d843699e951203025e50173be13fb829e8f32159
Darji Aymane <aymanedarji7@gmail.com>
2023-01-11 00:03:48 +0100

    Add methods to the controller createUser updateUser deleteUser getAllUsers updateUserStatus

--- src/main/java/app/project/youbook/controllers/AdminController.java
+++ src/main/java/app/project/youbook/controllers/AdminController.java
@@ -1,6 +1,7 @@
 package app.project.youbook.controllers;
 
 import app.project.youbook.domain.Hotel;
+import app.project.youbook.domain.User;
 import app.project.youbook.services.Dto.ResponseDto;
 import app.project.youbook.services.HotelService;
 import app.project.youbook.services.UserService;
@@ -51,6 +52,43 @@ public class AdminController {
         if (hotel != null){
             ResponseDto updatedHotel = hotelService.updateStatus(id, hotel.getStatus());
             return ResponseEntity.ok(updatedHotel);
+        }
+        return ResponseEntity.badRequest();
+    }
+
+    /*
+     User Management
+     */
+
+    @PostMapping("/users")
+    public ResponseEntity<ResponseDto> createUser(@RequestBody User user) {
+        ResponseDto createdUser = userService.save(user);
+        return ResponseEntity.ok(createdUser);
+    }
+
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<ResponseDto> updateUser(@PathVariable Long id, @RequestBody User user) {
+        ResponseDto updatedUser = userService.update(id, user);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+    @GetMapping("/users")
+    public ResponseEntity<ResponseDto> getAllUsers() {
+        ResponseDto users = userService.findAll();
+        return ResponseEntity.ok(users);
+    }
+
+    @PutMapping("/users/{id}/status")
+    public Object updateUserStatus(@PathVariable Long id, @RequestBody User user) {
+        if (user != null){
+            ResponseDto updatedUser = userService.updateStatus(id, user.getStatus().toString());
+            return ResponseEntity.ok(updatedUser);
         }
         return ResponseEntity.badRequest();
     }
